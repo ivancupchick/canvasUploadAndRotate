b7e1676cf8941191041aaec697e3681d4bd86608
Ivan Kupchyk <ivan.kupchic@gmail.com>
2019-02-28 22:51:19 +0300

    optimized code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function getId(id) {
+function getById(id) {
   return document.getElementById(id);
 }
 
 class UI {
   constructor() {
-    this.video = getId('video');
-    this.photoCanvas = getId('photoCanvas');
+    this.video = getById('video');
+    this.photoCanvas = getById('photoCanvas');
     this.photoContext = this.photoCanvas.getContext('2d');
-    this.photo = getId('photo');
-    this.buttonCapture = getId('buttonCapture');
-    this.mainCanvas = getId('mainCanvas');
+    this.photo = getById('photo');
+    this.buttonCapture = getById('buttonCapture');
+    this.mainCanvas = getById('mainCanvas');
     this.mainContext = this.mainCanvas.getContext('2d');
     this.isDown = false;
     this.previouslyAngle = 0;
@@ -20,19 +20,27 @@ class UI {
     };
   }
 
-  clearMainCanvas() {
+  rotatePhotoCanvas() {
     this.mainContext.translate(0, 0);
     this.mainContext.fillStyle = "rgb(221, 221, 221)";
     this.mainContext.fillRect(0, 0, this.mainCanvas.offsetWidth, this.mainCanvas.offsetHeight);
     this.mainContext.fill();
     this.mainContext.save();
     this.mainContext.translate(this.centerOfMainCanvas.x, this.centerOfMainCanvas.y);
-  }
 
-  rotatePhotoCanvas() {
     this.mainContext.rotate(this.angle);
     this.mainContext.drawImage(this.photoCanvas, -(this.photoCanvas.width / 2), -(this.photoCanvas.height / 2));
     this.mainContext.restore();
+  }
+
+  calculateAngle(event) {
+    let offset = {
+      dx: event.offsetX - this.centerOfMainCanvas.x,
+      dy: event.offsetY - this.centerOfMainCanvas.y
+    };
+
+    this.angle = Math.atan(offset.dy / offset.dx);
+    if (offset.dx < 0) this.angle += Math.PI;
   }
 }
 
@@ -43,36 +51,27 @@ window.onload = () => {
   navigator.mozGetUserMedia || navigator.msGetUserMedia;
   
   navigator.getMedia({
-    video: true,
-    audio: false
-  },
-  (stream) => {
-    ui.video.srcObject = stream;
-    ui.video.play();
-  }, 
-  (error) => {
-    console.log(error.message);
+      video: true,
+      audio: false
+    },
+    (stream) => {
+      ui.video.srcObject = stream;
+      ui.video.play();
+    }, 
+    (error) => {
+      console.log(error.message);
   });
 
   ui.buttonCapture.addEventListener('click', function() {
     ui.photoContext.drawImage(ui.video, 0, 0, 400, 300);
     ui.photo.setAttribute('src', ui.photoCanvas.toDataURL('image/png'));
-    ui.clearMainCanvas();
+
     ui.rotatePhotoCanvas(); 
   });
-  
+
   ui.mainCanvas.addEventListener('mousedown', event => {
     ui.isDown = true;
-    
-    let offset = {
-      dx: event.offsetX - ui.centerOfMainCanvas.x,
-      dy: event.offsetY - ui.centerOfMainCanvas.y
-    };
-    
-    ui.angle = Math.atan(offset.dy / offset.dx);
-
-    if (offset.dx < 0) ui.angle += Math.PI;
-    
+    ui.calculateAngle(event);
     ui.previouslyAngle = ui.previouslyAngle - ui.angle;
   })
 
@@ -91,17 +90,7 @@ window.onload = () => {
       return;
     }
 
-    ui.clearMainCanvas();
-
-    let offset = {
-      dx: event.offsetX - ui.centerOfMainCanvas.x,
-      dy: event.offsetY - ui.centerOfMainCanvas.y
-    };
-    
-    ui.angle = Math.atan(offset.dy / offset.dx);
-
-    if (offset.dx < 0) ui.angle += Math.PI;
-
+    ui.calculateAngle(event);
     ui.angle = ui.previouslyAngle + ui.angle;
 
     ui.rotatePhotoCanvas();
